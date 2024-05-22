6b9c3bd469d57e63229bf904e4bf01449f2904af
antip4d <149592934+antip4d@users.noreply.github.com>
2024-05-22 19:04:16 +0300

    Update api-hooks.js

--- app/api/api-hooks.js
+++ app/api/api-hooks.js
@@ -4,20 +4,14 @@ import { getNormalizedGamesDataByCategory, isResponseOk } from './api-utils'
 export const useGetDataByCategory = (endpoint, category) => {
   const [data, setData] = useState(null)
 
+ export const useGetDataByCategory = (endpoint, category) => {
+  const [data, setData] = useState(null)
   useEffect(() => {
     async function fetchData() {
-      const response = await getNormalizedGamesDataByCategory(endpoint, category)
-
-
-      if (isResponseOk(response)) {
-        setData(response)
-      } else {
-        // handle error
-      }
+      const data = await getNormalizedGamesDataByCategory(endpoint, category)
+      setData(data)
     }
-
     fetchData()
   }, [])
-
   return data
-};
+}
